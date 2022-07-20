3ad4c3fa2806b7d237a78d959585283c6a6bb64f
Gabriel Vieira dos Santos <gabrieldepiata@outlook.com>
2022-07-19 23:10:40 -0300

    filtes

---
+++ src/components/Filters/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export function Filters(){
+    return <div>
+        <input 
+            placeholder="Pesquisa"
+        />
+        <input 
+            type="number"
+            placeholder="Preço mínimo"
+        />
+        <input
+             type="number"
+             placeholder="Preço máximo"
+        />
+
+        
+        
+        
+    </div>
+    
+    
+}
