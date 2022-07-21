8a87581819a73eeed389b5a4767f62027ff57925
felipe_alcantara <fmalcantara@hotmail.com>
2022-07-20 23:37:38 -0300

    Home atualizada

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import CardHome from './Components/Home/CardHome'
-// import satList from '../sats.json'
-
+import { satList } from './mocDados'
+import { Header, Header2, Container } from '../src/Components/Home/styledHome'
 function App() {
-  
-  const [itens, setItens]= useState()
+  const [itens, setItens] = useState(satList)
+
+  const dadosRenderizados = itens.map(item => {
+    return <CardHome img={item.img} nome={item.name} valor={item.price} />
+  })
 
   return (
     <>
-    
-    <CardHome 
-    itens={itens} 
-    
-    />
+      <Header>
+        <span>Quantidade de Produtos: </span>
+        <div>
+          <label>Ordenação</label>
+          <select>
+            <option>Crescente</option>
+            <option>Decrescente</option>
+          </select>
+        </div>
+      </Header>
+      
+      <Container>{dadosRenderizados}</Container>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/Home/CardHome.js
+++ src/Components/Home/CardHome.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {Header, Container, ContainerPai, Lado , BoxCard} from '../Home/styledHome'
 
 function CardHome (props){
   return(
-    <div>
-    <img src= {props.foto} alt="Satelite"/>
-    <span>{props.nome} </span>
-    <span>{props.valor}</span>
-    <button onClick={props.handlesAddSat}> Adicionar</button>
-    </div>
+    <>
+    
+   
+    <BoxCard>
+        <img src= {props.img} alt="Satelite"/>
+        <p>{props.nome} </p>
+        <p>R$:{props.valor}</p>
+        <button> Adicionar</button>
+    </BoxCard>
+   
+    </>
   )
 }
 
