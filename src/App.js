ab81a5129aef63b3ea4a9f2e9695f1ebf85cd9f8
Antonio Junior <antoniojunior_95@hotmail.com>
2022-07-22 19:33:12 -0300

    carrinho-conflitos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 
-
+import React, { useState } from 'react'
+import CardHome from './Components/Home/CardHome'
+import { satList } from './mocDados'
+import { Header, Header2, Container } from '../src/Components/Home/styledHome'
 
 
 
 function App() {
   return (
-    <div>
+    <>
+    
+    <Header>
       
-     
-    </div>
+      <span>Quantidade de Produtos: </span>
+      <div>
+        <label>Ordenação</label>
+        <select>
+          <option>Crescente</option>
+          <option>Decrescente</option>
+        </select>
+      </div>
+    </Header>
+    
+    <Container>{dadosRenderizados}</Container>
+  
+  </>
   );
 }
 
