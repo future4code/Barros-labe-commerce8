cb34842b2eb36be07993f90321f66fb366f53c39
Antonio Junior <antoniojunior_95@hotmail.com>
2022-07-22 19:56:45 -0300

    carrinho-atualizado-semi-pronto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import CardHome from './Components/Home/CardHome'
 import { satList } from './mocDados'
 import { Header, Header2, Container } from '../src/Components/Home/styledHome'
+import Carrinho from './Components/Carrinho/Carrinho'
 
 
 
@@ -14,27 +15,29 @@ function App() {
   })
 
     
+  
   return (
+  
 
     <>
+    <Carrinho/>
     
-      <Header>
-        
-        <span>Quantidade de Produtos: </span>
-        <div>
-          <label>Ordenação</label>
-          <select>
-            <option>Crescente</option>
-            <option>Decrescente</option>
-          </select>
-        </div>
-      </Header>
+    <Header>
       
-      <Container>{dadosRenderizados}</Container>
+      <span>Quantidade de Produtos: </span>
+      <div>
+        <label>Ordenação</label>
+        <select>
+          <option>Crescente</option>
+          <option>Decrescente</option>
+        </select>
+      </div>
+    </Header>
     
-    </>
-  )
-
+    <Container>{dadosRenderizados}</Container>
+  
+  </>
+  );
 }
 
-export default App
+export default App;
